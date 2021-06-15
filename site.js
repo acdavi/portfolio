7b6685220d36cc3e40d03b1352b3aefdde5b8389
acdavi <daviaraujocandido@gmail.com>
2021-06-15 10:57:55 -0300

    Putting site up with Node.JS/Express.JS

---
+++ site.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = 3000;
+const HOST = 'localhost'
+const site = express();
+const path = require('path');
+
+site.use(express.static(path.join(__dirname, 'public')))
+
+
+
+site.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+site.listen(PORT, HOST, ()=>{
+    console.log('Site UP');
+})
